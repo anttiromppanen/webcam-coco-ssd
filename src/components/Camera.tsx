8ad0dac17d49b43f8613e4d40927a708e53004e8
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-13 10:12:10 +0300

    camera toggle test

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -3,7 +3,13 @@ import Webcam from "react-webcam";
 import styles from "./Camera.module.css";
 
 const Camera = forwardRef((_props, ref) => {
-  return <Webcam ref={ref as RefObject<Webcam>} className={styles.camera_sm} />;
+  return (
+    <Webcam
+      ref={ref as RefObject<Webcam>}
+      videoConstraints={{ facingMode: "user" }}
+      className={styles.camera_sm}
+    />
+  );
 });
 
 export default Camera;
