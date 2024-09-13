87a45f11eb17380f1cd3b64860891811cc6cdb88
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-13 13:50:37 +0300

    back camera toggle test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Webcam from "react-webcam";
 import Camera from "./components/Camera";
 import ObjectDetectionCanvas from "./components/ObjectDetectionCanvas";
 import useObjectDetection from "./hooks/useObjectDetection";
 import LoadingScreen from "./components/LoadingScreen";
+import { CameraIcon } from "@heroicons/react/16/solid";
 
 function App() {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  // const [cameraFacingMode, setCameraFacingMode] = useState<
-  //   "user" | "environment"
-  // >("user");
+  const [cameraFacingMode, setCameraFacingMode] = useState<
+    "user" | "environment"
+  >("user");
 
   const { isModelLoading } = useObjectDetection(webcamRef, canvasRef);
 
@@ -31,7 +32,7 @@ function App() {
             initial={{ opacity: 1 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 2 }}
+            transition={{ duration: 1 }}
           >
             <LoadingScreen />
           </motion.div>
@@ -41,7 +42,7 @@ function App() {
             key="main"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
+            transition={{ duration: 2 }}
             style={{
               width: "100%",
               height: "100%",
@@ -53,17 +54,32 @@ function App() {
           >
             <button
               type="button"
+              onClick={() =>
+                setCameraFacingMode((state) =>
+                  state === "user" ? "environment" : "user"
+                )
+              }
               style={{
                 position: "fixed",
                 top: 20,
                 right: 20,
                 zIndex: 99999,
-                backgroundColor: "white",
+                backgroundColor: "rgba(255, 255, 255, 0.5)",
+                borderRadius: "100%",
+                border: "none",
+                cursor: "pointer",
               }}
             >
-              Switch Camera
+              <CameraIcon
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  color: "black",
+                  margin: "5px",
+                }}
+              />
             </button>
-            <Camera ref={webcamRef} />
+            <Camera ref={webcamRef} cameraFacingMode={cameraFacingMode} />
             <ObjectDetectionCanvas ref={canvasRef} />
           </motion.main>
         )}
--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -2,14 +2,21 @@ import { forwardRef, RefObject } from "react";
 import Webcam from "react-webcam";
 import styles from "./Camera.module.css";
 
-const Camera = forwardRef((_props, ref) => {
-  return (
-    <Webcam
-      ref={ref as RefObject<Webcam>}
-      videoConstraints={{ facingMode: "user" }}
-      className={styles.camera_sm}
-    />
-  );
-});
+const cameraModeSelector = {
+  user: "user",
+  environment: { exact: "environment" },
+};
+
+const Camera = forwardRef(
+  ({ cameraFacingMode }: { cameraFacingMode: "user" | "environment" }, ref) => {
+    return (
+      <Webcam
+        ref={ref as RefObject<Webcam>}
+        videoConstraints={{ facingMode: cameraModeSelector[cameraFacingMode] }}
+        className={styles.camera_sm}
+      />
+    );
+  }
+);
 
 export default Camera;
